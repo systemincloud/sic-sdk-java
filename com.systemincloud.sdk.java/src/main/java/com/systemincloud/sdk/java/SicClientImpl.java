74f5bb91153935cb1523de6cebc290f569114b5a
mji <marek.jagielski@gmail.com>
2014-02-21 13:52:07 +0100

    init rest service

--- com.systemincloud.sdk.java/src/main/java/com/systemincloud/sdk/java/SicClientImpl.java
+++ com.systemincloud.sdk.java/src/main/java/com/systemincloud/sdk/java/SicClientImpl.java
@@ -1,8 +1,13 @@
 package com.systemincloud.sdk.java;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
 import com.systemincloud.sdk.java.msg.Credentials;
 import com.systemincloud.sdk.java.msg.TestConnectionReq;
@@ -14,13 +19,22 @@ public class SicClientImpl implements SicClient {
     
     protected Credentials credentials;
     
-    protected WebTarget    service;
+    protected WebTarget service;
     
     public SicClientImpl(String accountNumber, String systemName, String systemKey) {
         this.credentials = new Credentials(accountNumber, systemName, systemKey);
+        this.service = initRestClient();
+    }
+
+    private WebTarget initRestClient() {
+        ClientConfig config = new ClientConfig();
+        config.register(MultiPartFeature.class);
+        Client client = ClientBuilder.newBuilder()
+                                     .withConfig(config)
+                                     .build();
+        return client.target("https://connector.systemincloud.com");
     }
 
     @Override public TestConnectionRsp testConnection() { return service.path(PATH).path(TestConnectionReq.class.toString().toLowerCase()).request(MediaType.APPLICATION_JSON)
                                                                         .post(Entity.entity(new TestConnectionReq(credentials), MediaType.APPLICATION_JSON), TestConnectionRsp.class); }
-
 }
