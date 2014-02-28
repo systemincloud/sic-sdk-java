9c5a0ea770fe7a4570f029b7aeb1036e4d4a1ec0
mji <marek.jagielski@gmail.com>
2014-02-28 12:05:38 +0100

    NewMachine returns MachineInfo

--- com.systemincloud.sdk.java.msg/src/main/java/com/systemincloud/sdk/java/msg/NewMachineRsp.java
+++ com.systemincloud.sdk.java.msg/src/main/java/com/systemincloud/sdk/java/msg/NewMachineRsp.java
@@ -1,22 +1,23 @@
 package com.systemincloud.sdk.java.msg;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class NewMachineRsp extends Response {
     
-    @XmlElement private List<MachineInfo> machines = new ArrayList<>();
+    @XmlElement MachineInfo machine;
     
     public NewMachineRsp() { }
+    
+    public NewMachineRsp(MachineInfo machine) {
+        this.machine = machine;
+    }
 
     public NewMachineRsp(boolean status, String cause) {
         super(status, cause);
     }
 
-    public List<MachineInfo> getMachines()                           { return machines; }
-    public void              setMachines(List<MachineInfo> machines) { this.machines = machines; }
+    public MachineInfo getMachine()                    { return machine; }
+    public void        setMachine(MachineInfo machine) { this.machine = machine; }
 }
